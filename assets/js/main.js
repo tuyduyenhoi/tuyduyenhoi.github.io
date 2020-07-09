d465d12a635547e878b166424373b8d41b957737
trucnx <chukku410@aol.com>
2020-07-09 14:36:50 +0700

    edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -135,7 +135,7 @@
 
 			// Toggle.
 				$navPanelToggle = $(
-					'<a href="#navPanel" id="navPanelToggle">Menu</a>'
+					'<a href="#navPanel" id="navPanelToggle">Tất cả</a>'
 				)
 					.appendTo($wrapper);
 
